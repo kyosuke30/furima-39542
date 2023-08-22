83222f96dcb8305f2ea5a19e326fa4c68e6e77a0
kyosuke30 <28132bu@gmail.com>
2023-08-22 16:01:03 +0900

    itemテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :fist_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
+
+  has_many :items
 end
