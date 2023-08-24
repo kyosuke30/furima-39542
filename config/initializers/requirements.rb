9d10cf4d3cc38cd2b933f6da2834c6044df488e1
kyosuke30 <28132bu@gmail.com>
2023-08-24 15:46:51 +0900

    修正

--- config/initializers/requirements.rb
+++ config/initializers/requirements.rb
@@ -1,3 +1,3 @@
-require Rails.root.join("app/models/Salesstatus.rb")
+require Rails.root.join("app/models/salesstatus.rb")
 require Rails.root.join("app/models/shippingfeestatus.rb")
 require Rails.root.join("app/models/scheduleddelivery.rb")
