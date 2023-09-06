da5a896e56edabbc75ee2b500946709e69fc63b2
kyosuke30 <28132bu@gmail.com>
2023-09-06 15:28:41 +0900

    テストコード修正

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe OrderForm, type: :model do
   before do
     @order_form = FactoryBot.build(:order_form)
+    @order_form.user_id = 1
+    @order_form.item_id = 1
   end
 
   describe '配送先情報の保存' do
@@ -93,6 +95,11 @@ RSpec.describe OrderForm, type: :model do
       end
       it '電話番号にハイフンがあると保存できないこと' do
         @order_form.phone_number = '123 - 1234 - 1234'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が9桁以下あでは保存できないこと' do
+        @order_form.phone_number = 12_345_678
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
